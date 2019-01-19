4b34fbf7061d275f8e926b7f56367fcb1cb23f6a
wangjx <156060158@qq.com>
2019-01-19 23:13:48 +0800

    update .gitignore、

--- Nuget/pack.bat
+++
@@ -1,6 +0,0 @@
-set currdir=%~dp0
-for %%i in (DataGate.Com DataGate.App DataGate.Api DataGate.Tests DataGate) do (
-    "%currdir%nuget.exe" pack "%currdir%..\%%i" -OutputDirectory  "%currdir%packages" -Properties Configuration=Release
-);
-set currdir=
-pause
