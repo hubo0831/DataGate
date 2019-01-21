000846fc9c0dcc5cee3aca7932ae37fd9456b93d
wangjx <156060158@qq.com>
2019-01-22 00:24:06 +0800

    remove font-awesome from  index.js

--- DataGate.Web/src/index.base.js
+++ DataGate.Web/src/index.base.js
@@ -2,7 +2,6 @@
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import appConfig from './appConfig.js'
 
