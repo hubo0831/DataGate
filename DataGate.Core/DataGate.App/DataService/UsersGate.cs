d15fbdfdddb5e6be0351878f9390589a045f6c45
wangjx <156060158@qq.com>
2019-03-01 01:32:30 +0800

    change password + salt logic

--- DataGate.Core/DataGate.App/DataService/UsersGate.cs
+++ DataGate.Core/DataGate.App/DataService/UsersGate.cs
@@ -8,26 +8,47 @@ namespace DataGate.App.DataService
 {
     public class UsersGate : ISubmitDataGate
     {
+        string GetLUKey(IDictionary<string, object> ps, string key)
+        {
+            return ps.Keys.FirstOrDefault(k => k.Equals(key, StringComparison.OrdinalIgnoreCase));
+        }
+
         public void OnAdd(DataGateKey gkey, IDictionary<string, object> ps)
         {
-            if (!ps.TryGetValue("Password", out object pwd) && !ps.TryGetValue("password", out pwd))
+            string passKey = GetLUKey(ps, "Password");
+            string pwd = null;
+            if (passKey != null)
+            {
+                pwd = (string)ps[passKey];
+            }
+            else
+            {
+                passKey = "Password";
+            }
+            if (pwd.IsEmpty())
             {
                 pwd = "123456";
             }
+            gkey.MainTable.Fields.Add(new FieldMeta { Name = "Password" });
+            gkey.MainTable.Fields.Add(new FieldMeta { Name = "PasswordSalt" });
             ps["PasswordSalt"] = CommOp.NewId();
 
-            ps["Password"] = Encryption.MD5(pwd + (string)ps["PasswordSalt"]);
+            ps[passKey] = Encryption.MD5(pwd + (string)ps["PasswordSalt"]);
             ps["createDate"] = DateTime.Now;
         }
 
         public void OnChange(DataGateKey gkey, IDictionary<string, object> ps)
         {
-            if (!ps.TryGetValue("Password", out object pwd) && !ps.TryGetValue("password", out pwd))
+            string passKey = GetLUKey(ps, "Password");
+            if (passKey == null)
             {
                 return;
             }
+            string pwd = (string)ps[passKey];
             ps["PasswordSalt"] = CommOp.NewId();
-            ps["Password"] = Encryption.MD5(pwd + (string)ps["PasswordSalt"]);
+            ps[passKey] = Encryption.MD5(pwd + (string)ps["PasswordSalt"]);
+            gkey.MainTable.Fields.Add(new FieldMeta { Name = "Password" });
+            gkey.MainTable.Fields.Add(new FieldMeta { Name = "PasswordSalt" });
         }
 
         public void OnRemove(DataGateKey gkey, IDictionary<string, object> ps)
